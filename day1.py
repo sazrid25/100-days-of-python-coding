c148bafab81313ab24e65e6ff0b47860d426c73b
sazrid25 <sazrid25@gmail.com>
2025-01-16 20:09:04 +0600

    first commit

---
+++ day1.py
@@ -0,0 +1,11 @@
+glass1 = "milk"
+glass2 = "juice"
+
+print(glass1+glass2)
+
+temp = glass1
+glass1 = glass2
+glass2 = temp
+
+print(glass1+glass2)
+
---
+++ project1.py
@@ -0,0 +1,10 @@
+# Band Name Generator
+
+print("Welcome to the Band Name Generator.")
+
+var1 = input("What is your favourite color?\n")
+
+var2 = input("What is your favourite season?\n")
+
+
+print("Your Band Name can be: " + var1 + " " + var2)
